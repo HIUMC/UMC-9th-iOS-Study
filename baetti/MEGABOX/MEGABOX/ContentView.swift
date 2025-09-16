351fbdf3105a0d9869311a686627bf3af7082e88
Junghwan <hwane2000@gmail.com>
2025-09-17 02:48:34 +0900

    feat: SplashView 추가

---
+++ baetti/MEGABOX/MEGABOX/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MEGABOX
+//
+//  Created by 박정환 on 9/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ baetti/MEGABOX/MEGABOX/LoginView.swift
@@ -0,0 +1,8 @@
+//
+//  LoginView.swift
+//  MEGABOX
+//
+//  Created by 박정환 on 9/17/25.
+//
+
+import Foundation
