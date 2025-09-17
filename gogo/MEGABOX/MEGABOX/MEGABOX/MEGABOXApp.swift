39de1a4166bd9fa561bb8631b7f04be46827eb0a
gogo <daehcjf@gmail.com>
2025-09-17 16:18:44 +0900

    [Feat] SplashView

---
+++ gogo/MEGABOX/MEGABOX/MEGABOX/MEGABOXApp.swift
@@ -0,0 +1,17 @@
+//
+//  MEGABOXApp.swift
+//  MEGABOX
+//
+//  Created by 고석현 on 9/17/25.
+//
+
+import SwiftUI
+
+@main
+struct MEGABOXApp: App {
+    var body: some Scene {
+        WindowGroup {
+         LoginView()
+        }
+    }
+}
