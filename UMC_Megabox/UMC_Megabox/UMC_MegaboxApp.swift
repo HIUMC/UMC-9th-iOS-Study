e59cece06448b6275ed7f15b07ebf275defa2b6e
21g <leehan0311@gmail.com>
2025-09-17 13:33:12 +0900

    feat: 최초 파일 커밋

---
+++ UMC_Megabox/UMC_Megabox/UMC_MegaboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  UMC_MegaboxApp.swift
+//  UMC_Megabox
+//
+//  Created by OOng E on 9/16/25.
+//
+
+import SwiftUI
+
+@main
+struct UMC_MegaboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
