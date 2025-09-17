337797edb36e72670310ee9277cf92977896b3d1
Junghwan <hwane2000@gmail.com>
2025-09-17 14:22:38 +0900

    feat: LoginView 추가

--- baetti/MEGABOX/MEGABOX/LoginView.swift
+++ baetti/MEGABOX/MEGABOX/LoginView.swift
@@ -5,4 +5,89 @@
 //  Created by 박정환 on 9/17/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        VStack {
+            navigationBar
+            Spacer()
+
+            InputSection
+                .padding(.bottom, 75)
+
+            LoginButton
+                .padding(.bottom, 35)
+
+            SocialLoginSection
+                .padding(.bottom, 39)
+
+            BannerSection
+        }
+        .padding(.horizontal, 16)
+    }
+
+    private var navigationBar: some View {
+        HStack{
+            Text("로그인")
+                .font(.semiBold24)
+        }
+    }
+    
+    private var InputSection: some View {
+        VStack(alignment:.leading){
+            
+            Text("아이디")
+                .font(.medium16)
+                .foregroundColor(.gray03)
+                .padding(.bottom, 4)
+            
+            Divider()
+                .padding(.bottom, 40)
+            
+            Text("비밀번호")
+                .font(.medium16)
+                .foregroundColor(.gray03)
+                .padding(.bottom, 4)
+            Divider()
+        }
+    }
+    
+    private var LoginButton: some View {
+        VStack(alignment:.center) {
+            Button(action: {}) {
+                Text("로그인")
+                    .font(.bold18)
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity, maxHeight: 54)
+                    .background(Color.purple03)
+                    .cornerRadius(10)
+            }
+            .padding(.bottom, 17)
+            
+            Button(action: {}) {
+                Text("회원가입")
+                    .font(.medium13)
+                    .foregroundColor(.gray03)
+            }
+        }
+    }
+    
+    private var SocialLoginSection: some View {
+        HStack(spacing: 73) {
+            Image("naverLogin")
+            Image("kakaoLogin")
+            Image("appleLogin")
+        }
+    }
+    
+    private var BannerSection: some View {
+        Image("loginBanner")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+    }
+}
+
+#Preview {
+    LoginView()
+}
