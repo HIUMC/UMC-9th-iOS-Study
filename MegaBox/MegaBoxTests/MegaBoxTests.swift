f0b1b4066bf6e4312b8155e1f78f15e90e83abf5
Byeongseon-Park <parkbsbsbsbsbs@gmail.com>
2025-09-18 15:56:54 +0900

    [고니/박병선] 1주차 과제 제출
    1주차 과제 제출

---
+++ MegaBox/MegaBoxTests/MegaBoxTests.swift
@@ -0,0 +1,17 @@
+//
+//  MegaBoxTests.swift
+//  MegaBoxTests
+//
+//  Created by 박병선 on 9/16/25.
+//
+
+import Testing
+@testable import MegaBox
+
+struct MegaBoxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
