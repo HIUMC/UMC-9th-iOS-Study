56c0d0f87729333b2fcd7360233e4a20202c0301
gogo <daehcjf@gmail.com>
2025-10-02 19:06:09 +0900

    [Feat] 과제 끝

--- gogo/MEGABOX/MEGABOX/MEGABOX/Views/HomeView.swift
+++ gogo/MEGABOX/MEGABOX/MEGABOX/Views/HomeView.swift
@@ -8,11 +8,7 @@
 import SwiftUI
 
 
-//MARK: -무비차트/상영예정 탭
-enum MovieTab: String, CaseIterable {
-    case chart = "무비차트"
-    case upcoming = "상영예정"
-}
+
 
 struct HomeView: View {
     @Environment(NavigationRouter.self) var router
@@ -33,22 +29,29 @@ struct HomeView: View {
                 }
             }
         }
+       
     }
 }
 
+//MARK: - 홈 메인 뷰 extension으로 처리 ("홈" 눌렀을때만 나오도록)
 private extension HomeView {
     var homeMainView: some View {
         ScrollView(.vertical, showsIndicators: false) {
             TopButtonView(select: $tab)
             switch tab {
             case .chart:
-                MovieFeedView(movies: viewModel.movies, onSelect: {movie in
+                MovieScrollView(movies: viewModel.movies, onSelect: {movie in
                     router.push(.movieDetail(movie: movie))
                 })
             case .upcoming:
-                EmptyView()
+                Text("상영예정 하위뷰 작성 예정")
+                //TODO: - 상영예정 하위뷰
+            
             }
+            TopFeedView
+            BottomFeedView
         }
+      
     }
 }
 
@@ -98,6 +101,39 @@ struct TopBarView: View {
     }
 }
 
+
+
+//MARK: -무비차트/상영예정 탭 Enum
+enum MovieTab: String, CaseIterable {
+    case chart = "무비차트"
+    case upcoming = "상영예정"
+}
+
+//MARK: - 무비차트/상영예정
+struct TopButtonView: View {
+    @Binding var select: MovieTab
+    var body: some View {
+        HStack(spacing: 10) {
+            ForEach(MovieTab.allCases, id: \.self) { tab in
+                Button {
+                    select = tab
+                } label: {
+                    Text(tab.rawValue)
+                        .font(.PretendardMedium(size: 14))
+                        .frame(width: 84, height: 38, alignment: .center)
+                        .background(
+                            Capsule()
+                                .fill(select == tab ? Color.gray08 : Color.gray02)
+                        )
+                        .foregroundStyle(select == tab ? .white : .gray04)
+                }
+            }
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.horizontal, 20)
+        .padding(.vertical, 6)
+    }
+}
 //MARK: - 무비 카드 (하위 블럭)
 struct MovieCard: View {
     let movieModel: MovieModel
@@ -125,61 +161,24 @@ struct MovieCard: View {
                 }
                 .padding(.bottom, 8)
             }
+            Spacer().frame(height: 4)
 
             Text(movieModel.title)
                 .font(.PretendardBold(size:22))
+            Spacer().frame(height: 2)
             Text("누적관객수 \(movieModel.countAudience)")
                 .font(.PretendardMedium(size:18))
         }
     }
 }
 
-//MARK: - 무비차트 가로 스크롤 뷰
-struct MovieScrollChart: View {
+//MARK: - 무비차트 가로 스크롤뷰
+struct MovieScrollView: View {
     let movies: [MovieModel]
     let onSelect: (MovieModel) -> Void
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
-            HStack(spacing: 24) {
-                ForEach(movies) { movie in
-                    MovieCard(movieModel: movie) {
-                        onSelect(movie)
-                    }
-                }
-            }
-        }
-        .frame(height: 320)
-    }
-}
-
-//MARK: - TopButtonView
-struct TopButtonView: View {
-    @Binding var select: MovieTab
-    var body: some View {
-        HStack(spacing: 0) {
-            Spacer().frame(width: 20)
-            ForEach(MovieTab.allCases, id: \.self) { tab in
-                Button {
-                    select = tab
-                } label: {
-                    Text(tab.rawValue)
-                        .foregroundStyle(select == tab ? .black : .gray04)
-                        .font(.PretendardSemiBold(size: 24))
-                }
-                Spacer()
-            }
-        }
-        .padding(.bottom, 16)
-    }
-}
-
-//MARK: - MovieFeedView
-struct MovieFeedView: View {
-    let movies: [MovieModel]
-    let onSelect: (MovieModel) -> Void
-    var body: some View {
-        ScrollView(.horizontal, showsIndicators: false) {
-            HStack(spacing: 24) {
+            HStack(spacing: 15) {
                 ForEach(movies) { movie in
                     MovieCard(movieModel: movie) {
                         onSelect(movie)
@@ -187,10 +186,82 @@ struct MovieFeedView: View {
                 }
             }
             .padding(.horizontal, 20)
+            
         }
         .frame(height: 320)
     }
 }
+
+
+//MARK: -알고보면 더 재밌는 무비피드
+private var TopFeedView: some View {
+    VStack(spacing: 0) {
+        HStack {
+            Text("알고보면 더 재밌는 무비피드")
+                .font(.PretendardBold(size:24))
+            Spacer()
+            Button(action: {
+                //TODO: -무비 피드이동
+            }) {
+                Image("right")
+                    .tint(.gray08)
+                    .frame(width: 50, height: 50)
+//                    .glassEffect(.clear.interactive())
+            }
+        }
+        Image(.movieFeed1)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: .infinity)
+            .padding(.bottom, 25)
+    }
+    .padding(.horizontal, 20)
+    
+}
+
+//MARK: -하단 영화 피드
+private var BottomFeedView: some View {
+    VStack(alignment: .leading, spacing: 0) {
+        HStack(spacing: 12) {
+            Image(.movieFeed2)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 100)
+            VStack(alignment: .leading, spacing: 0) {
+                Text("9월, 메가박스의 영화들(1) - 명작들의 재개봉")
+                    .font(.PretendardSemiBold(size:18))
+                    .foregroundStyle(.black)
+                    .padding(.bottom, 25)
+                Text("<모노노케 히메>,<퍼펙트 블루>")
+                    .font(.PretendardSemiBold(size:13))
+                    .foregroundStyle(.gray03)
+            }
+        }
+        .padding(.horizontal, 20)
+        .padding(.bottom, 39)
+
+        HStack(spacing: 12) {
+            Image(.movieFeed3)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 100)
+            VStack(alignment: .leading, spacing: 0) {
+                Text("메가박스 오리지널 티켓 Re.37 <얼굴>")
+                    .font(.PretendardSemiBold(size:18))
+                    .foregroundStyle(.black)
+                    .padding(.bottom, 25)
+                Text("영화 속 양극적인 감정의 대비")
+                    .font(.PretendardSemiBold(size:13))
+                    .foregroundStyle(.gray03)
+            }
+        }
+        .padding(.horizontal, 20)
+    }
+ 
+}
+
+
+
 
 
 #Preview {
@@ -198,4 +269,3 @@ struct MovieFeedView: View {
         .environment(NavigationRouter())
         .environment(MovieViewModel())
 }
-
