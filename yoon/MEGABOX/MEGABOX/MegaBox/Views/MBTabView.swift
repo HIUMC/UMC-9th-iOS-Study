ecf1b9114e40be498d0eef595fc8eef1bf45dc3f
정승윤 <jswun123@g.hongik.ac.kr>
2025-10-05 23:19:05 +0900

    2

---
+++ yoon/MEGABOX/MEGABOX/MegaBox/Views/MBTabView.swift
@@ -0,0 +1,41 @@
+//
+//  TabView.swift
+//  week1_homework
+//
+//  Created by 정승윤 on 9/30/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct MBTabView: View {
+    @Environment(NavigationRouter.self) var router
+    @Environment(MovieViewModel.self) var viewModel
+    var body: some View {
+        TabView {
+            Tab("홈", systemImage: "house.fill") {
+                HomeView()
+                    .environment(router)
+                    .environment(viewModel)
+            }
+
+            Tab("바로 예매", systemImage: "play.laptopcomputer") {
+                Text("Sent View")
+            }
+
+            Tab("모바일 오더", systemImage: "popcorn") {
+                Text("Account View")
+            }
+            
+            Tab("마이 페이지", systemImage: "person") {
+                UserInfoView()
+                  }
+        }
+    }
+}
+// 그냥 탭뷰로 하니 충돌 남
+#Preview {
+    MBTabView()
+        .environment(NavigationRouter())
+        .environment(MovieViewModel())
+}
---
+++ yoon/MEGABOX/MEGABOX/MegaBox/Views/MovieInfoView.swift
@@ -0,0 +1,93 @@
+//
+//  MovieinfoView.swift
+//  week1_homework
+//
+//  Created by 정승윤 on 10/1/25.
+//
+
+import Foundation
+import SwiftUI
+
+// 패스뷰를 통해서 들어오면 패딩이 적용이 안됌
+// NavigationStack -> 자식 뷰로 갈때 레이아웃이 달라지기 때문
+// 패스 뷰로 들어왔을 때 레이아웃 깨지는걸 어떻게 해야 할지 모르겠음.,
+struct MovieInfoView: View {
+    let movie: MovieModel
+    @Environment(\.dismiss) var dismiss
+    var body: some View {
+        VStack {
+            NavigationBar
+            MovieInfo
+            Spacer().frame(height: 35)
+            MovieDetailBar
+            Spacer().frame(height: 17)
+            MovieDetail
+            Spacer()
+        }.padding(.horizontal,16)
+    }
+    
+    private var NavigationBar: some View {
+        VStack{
+            HStack{
+                Button(action: {dismiss()
+                }){Text(Image(systemName: "arrow.left")).foregroundStyle(.black)}
+                    
+                Spacer()
+                
+                Text(movie.name)
+                        .font(.Pretendardmedium18)
+                        .padding(.trailing,162)
+                }.frame(width: 408)
+            }.padding(.horizontal,16)
+        }
+    
+    private var MovieInfo: some View {
+        VStack(alignment: .center){
+            Image(movie.secPosterName).frame(height:248)
+            Spacer().frame(height: 9)
+            Text(movie.name).font(.PretendardBold24)
+            Text(movie.engname).foregroundStyle(.gray03).font(.PretendardsemiBold14)
+            Text("최고가 되지 못한 전설 VS 최고가 되고 싶은 루키 한때 주목받는 유망주였지만 끔찍한 사고로 F1에서 우승하지 못하고 한순간에 추락한 드라이버 ‘손; 헤이스'(브래드 피트). 그의 오랜 동료인 ‘루벤 세르반테스'(하비에르 바르뎀)에게 레이싱 복귀를 제안받으며 최하위 팀인 APGXP에 합류한다.").font(.PretendardsemiBold18).foregroundStyle(.gray03)
+        }
+    }
+    private var MovieDetailBar: some View {
+        HStack{
+            VStack{
+                Text("상세정보").font(.PretendardBold22)
+                Rectangle()
+                    .frame(height: 1)
+                    .frame(maxWidth: 220)
+                    .foregroundColor(.black)
+            }
+            
+            VStack{
+                Text("실관람평").font(.PretendardBold22)
+                Rectangle().frame(height: 1)
+                    .frame(maxWidth: 220)
+                    .foregroundColor(.black)
+            }   
+        }
+    }
+    private var MovieDetail: some View {
+        HStack{
+            Image(movie.posterName).resizable()
+                .frame(width: 100,height: 120)
+            VStack{
+                Text("12세 이상 관람가").font(.PretendardsemiBold13)
+                Spacer().frame(height:9)
+                Text("2025.06.25 개봉").font(.PretendardsemiBold13)
+            }
+            Spacer()
+        }
+    }
+}
+
+struct MovieInfoView_Preview: PreviewProvider {
+    static var previews: some View {
+        devicePreviews {
+            MovieInfoView(movie: MovieModel(posterName: "sample",secPosterName: "암거나",name:"영화제목",engname: "영어이름", performance: "100만명"))
+                .environment(NavigationRouter())
+                .environment(MovieViewModel())
+        }
+    }
+} // 현재 프리뷰는 데이터를 받은게 없으니까 정말 예시로 아무거나 들어가는 것이구나?
